24e8bd8e111f95643907df5086fde7ec0badb138
Vinh Le <vinhyuki1995@gmail.com>
2023-07-04 00:20:25 +0800

    Add Positive and Negative Login cases for Test Login Page

--- cypress/cypress/e2e/vinhyuki95-myPortfolio.cy.js
+++ cypress/cypress/e2e/vinhyuki95-myPortfolio.cy.js
@@ -1,9 +1,20 @@
-describe("Login page", () => {
+describe("Login", () => {
   beforeEach(() => {
-    cy.visit("https://practicetestautomation.com/practice-test-login");
+    cy.visit("https://practicetestautomation.com/practice-test-login/");
   });
-
-  it("Positive LogIn test", () => {
+  it("Positive Login", () => {
     cy.get("#username").type("student");
+    cy.get("#password").type("Password123");
+    cy.get("#submit").click();
+    cy.url().should("contain", "practicetestautomation.com");
+    cy.get("div").should("contain.text", "Congratulation");
+    cy.get("a").should("contain.text", "Log out");
+  });
+  it("Negative Login", () => {
+    cy.get("#username").type("incorrectUsername");
+    cy.get("#submit").type("incorrectPassword");
+    cy.get("#submit").click();
+    cy.get("#error").should("exist");
+    cy.get("*").should("contain.text", "Your username is invalid!");
   });
 });
