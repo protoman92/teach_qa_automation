0b9e9102ff347f86d1651576dff0ff001af786af
Vinh Le <vinhyuki1995@gmail.com>
2023-07-13 00:24:06 +0800

    Positive: complete to do

--- cypress/cypress/e2e/todos.cy.js
+++ cypress/cypress/e2e/todos.cy.js
@@ -5,8 +5,15 @@ describe("todos", () => {
   it("Positive: Add a new to do", () => {
     cy.get(".new-todo").type("go to update new phone number at DBS");
     cy.get(".new-todo").type("{enter}");
-    cy.get("*")
+    cy.get("label")
       .invoke("text")
       .should("equal", "go to update new phone number at DBS");
+  });
+  it("Positive: Complete to do when tick", () => {
+    cy.get(".new-todo").type("go to update new phone number at DBS");
+    cy.get(".new-todo").type("{enter}");
+    cy.get("li.completed").should("not.exist");
+    cy.get(".toggle").click();
+    cy.get("li.completed").should("exist");
   });
 });
