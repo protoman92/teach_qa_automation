13379f4e4222979b9ac17191669860842029f308
Vinh Le <vinhyuki1995@gmail.com>
2023-07-13 00:27:22 +0800

    Positive: add clear completed to do when click clear completed

--- cypress/cypress/e2e/todos.cy.js
+++ cypress/cypress/e2e/todos.cy.js
@@ -16,4 +16,12 @@ describe("todos", () => {
     cy.get(".toggle").click();
     cy.get("li.completed").should("exist");
   });
+  it("Positive: Clear completed to do when click clear completed", () => {
+    cy.get(".new-todo").type("go to update new phone number at DBS");
+    cy.get(".new-todo").type("{enter}");
+    cy.get(".toggle").click();
+    cy.get("li.completed").should("exist");
+    cy.get(".clear-completed").click();
+    cy.get("li.completed").should("not.exist");
+  });
 });
