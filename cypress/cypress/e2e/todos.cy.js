e0441fc89f05719b0a11558312662d3ba3d7c4c7
Vinh Le <vinhyuki1995@gmail.com>
2023-07-23 02:20:15 +0800

    Use For loop to select one or multiple checkboxes

--- cypress/cypress/e2e/todos.cy.js
+++ cypress/cypress/e2e/todos.cy.js
@@ -1,59 +1,47 @@
-// describe("todos", () => {
-//   beforeEach(() => {
-//     cy.visit("https://todomvc.com/examples/react/#/");
-//   });
-//   it("Positive: Add a new to do", () => {
-//     cy.get(".new-todo").type("go to update new phone number at DBS");
-//     cy.get(".new-todo").type("{enter}");
-//     cy.get("label")
-//       .invoke("text")
-//       .should("equal", "go to update new phone number at DBS");
-//   });
-//   it("Positive: Complete to do when tick", () => {
-//     cy.get(".new-todo").type("go to update new phone number at DBS");
-//     cy.get(".new-todo").type("{enter}");
-//     cy.get("li.completed").should("not.exist");
-//     cy.get(".toggle").click();
-//     cy.get("li.completed").should("exist");
-//   });
-//   it("Positive: Clear completed to do when click clear completed", () => {
-//     cy.get(".new-todo").type("go to update new phone number at DBS");
-//     cy.get(".new-todo").type("{enter}");
-//     cy.get(".toggle").click();
-//     cy.get("li.completed").should("exist");
-//     cy.get(".clear-completed").click();
-//     cy.get("li.completed").should("not.exist");
-//   });
-// });
 describe("todos", () => {
   beforeEach(() => {
     cy.visit("https://todomvc.com/examples/react/#/");
   });
-  beforeEach("", () => {
-    // cy.get(".new-todo").type("Task1: Read and analysis requirement");
-    // cy.get(".new-todo").type("{enter}");
-    // cy.get(".new-todo").type("Task2: Create test cases");
-    // cy.get(".new-todo").type("{enter}");
-    // cy.get(".new-todo").type("Task3: Execute test based on test cases");
-    // cy.get(".new-todo").type("{enter}");
-    // cy.get(".new-todo").type("Task4: Report bug");
-    // cy.get(".new-todo").type("{enter}");
 
+  beforeEach("list of to do", () => {
     let todosList = [
       "Task1: Read and analysis requirement",
       "Task2: Create test cases",
       "Task3: Execute test based on test cases",
       "Task4: Report bug",
     ];
-    let a = "";
     for (let i = 0; i < todosList.length; i = i + 1) {
       cy.get(".new-todo").type(todosList[i]);
       cy.get(".new-todo").type("{enter}");
     }
   });
-  it("Positive: Input data", () => {
-    cy.get(".todo-list li:nth-child(1) .view .destroy").click({
-      force: true,
-    });
+
+  // it("Check display  ", () => {
+  //   cy.get(".todo-list li:nth-child(1) .view .destroy").click({
+  //     force: true,
+  //   });
+  // });
+
+  //Check when select 1 checkbox
+  it("Check when select 1 checkbox", () => {
+    cy.get(".todo-list li:nth-child(1) .view .toggle").click();
+    cy.get(".todo-list li:nth-child(1)").should("have.class", "completed");
+  });
+
+  //Check when select multiple checkboxes
+  it("Check when select multiple checkboxes", () => {
+    for (let i = 1; i < 3; i = i + 1) {
+      cy.get(`li:nth-child(${i}) .view .toggle`).click();
+      cy.get(`li:nth-child(${i})`).should("have.class", "completed"); //use String Interpolation
+    }
+    // cy.get(".todo-list li:nth-child(1) .view .toggle").click();
+    // cy.get(".todo-list li:nth-child(2) .view .toggle").click();
+    // cy.get(".todo-list li").should("have.class", "completed");
+  });
+
+  //Click on toggle
+  it("Check function when click on dropdown list at the first time", () => {
+    cy.get("label[for=toggle-all]").click();
+    // cy.get("footer .filters li:nth-child(1) .selected").click(); //Check function when click on Active button
   });
 });
